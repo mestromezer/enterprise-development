2e9a4bc86dd52e6e2483477e925a1be7d4ac6167
Ivan Chumakov <global0908@mail.ru>
2024-12-01 10:39:30 +0300

    Врапер для автогенеренного клиента

---
+++ Pharmacies/Pharmacies.Client/OpenApiService/IPharmaciesApiWrapper.cs
@@ -0,0 +1,20 @@
+﻿namespace Pharmacies.Client.OpenApiService;
+
+public interface IPharmaciesApiWrapper
+{
+    Task<PharmacyDto> CreatePharmacy(PharmacyDto newPharmacy);
+    Task<PriceDto> CreatePrice(PriceDto newPrice);
+    Task<PositionDto> CreatePosition(PositionDto newPosition);
+    Task DeletePharmacy(int id);
+    Task DeletePrice(int id);
+    Task DeletePosition(int id);
+    Task<IList<PharmacyDto>> GetAllPharmacies();
+    Task<IList<PriceDto>> GetAllPrices();
+    Task<IList<PositionDto>> GetAllPositions();
+    Task<PharmacyDto> GetPharmacy(int id);
+    Task<PriceDto> GetPrice(int id);
+    Task<PositionDto> GetPosition(int id);
+    Task UpdatePharmacy(int id, PharmacyDto newPharmacy);
+    Task UpdatePrice(int id, PriceDto newPrice);
+    Task UpdatePosition(int id, PositionDto newPosition);
+}
